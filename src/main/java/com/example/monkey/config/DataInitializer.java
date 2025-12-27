b9c24c691ec9c0f5cc81cce9b06946396e3caf1b
haohaizi554 <haohaizi554@gamil.com>
2025-12-27 09:31:30 +0800

    增加

--- src/main/java/com/example/monkey/config/DataInitializer.java
+++ src/main/java/com/example/monkey/config/DataInitializer.java
@@ -9,7 +9,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class DataInitializer {
-
     @Bean
     public CommandLineRunner initData(AdminRepository adminRepository, PasswordEncoder passwordEncoder) {
         return args -> {
@@ -19,7 +18,6 @@ public class DataInitializer {
                 admin.setUsername("admin");
                 admin.setPassword(passwordEncoder.encode("123456"));
                 admin.setNickname("超级管理员");
-
                 adminRepository.save(admin);
                 System.out.println("【初始化】管理员创建成功！账号: admin / 密码: 123456");
             }
